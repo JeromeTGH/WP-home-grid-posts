93d3770139ba0e16639d16d341615fc016fabe30
JeromeTGH <jerome.tomski@gmx.fr>
2024-03-01 22:45:58 +0100

    Préparation basculement des selects, dans la page 'admin categories'

--- js/script.js
+++ js/script.js
@@ -11,3 +11,92 @@ const JTGH_WPHGP_handleClickOnCopyButton = () => {
         alert('Impossible de copier ce shortcode, désolé ...');
     })
 }
+
+// ==========================================
+// Function : JTGH_WPHGP_handleCategoriesBasc
+// ==========================================
+const JTGH_WPHGP_handleCategoriesBasc = (option) => {
+
+    // Pointage des select
+    const source_cat_select = document.getElementById("JTGH_WPHGP_categories_source");
+    const dest_cat_select = document.getElementById("JTGH_WPHGP_categories_dest");
+
+    // Chargement des données dans des tableaux, en vidant les selects au fur et à mesure
+    const tblSrc = [];
+    const tblDest = [];
+
+    for (let i=source_cat_select.options.length-1; i>=0; i--) {
+        tblSrc.push({
+            value: source_cat_select.options[i].value,
+            innerText: source_cat_select.options[i].innerText,
+            selected: source_cat_select.options[i].selected
+        })
+        source_cat_select.remove(i);
+    }
+    for (let i=dest_cat_select.options.length-1; i>=0; i--) {
+        tblDest.push({
+            value: dest_cat_select.options[i].value,
+            innerText: dest_cat_select.options[i].innerText,
+            selected: dest_cat_select.options[i].selected
+        })
+        dest_cat_select.remove(i);
+    }
+
+
+
+
+
+
+
+    // Logique de manipulation des tableaux
+    switch(option) {
+        case '>>':
+            // for(let src_opt in tblSrc) {
+            //     tblDest[src_opt] = tblSrc[src_opt];
+            //     delete tblSrc[src_opt];
+            // }
+            break;
+        case '>':
+            break;
+        case '<<':
+            // for(let dest_opt in tblDest) {
+            //     tblSrc[dest_opt] = tblDest[dest_opt];
+            //     delete tblDest[dest_opt];
+            // }
+            break;
+        case '<':
+            break;
+        default:
+            break;
+    }
+
+
+
+    // Tri alphabétique des tableaux, en fonction de la colonne "innerText"
+    const sortedTblSrc = Object.entries(tblSrc).sort((a, b) => {
+        if (a[1].innerText < b[1].innerText) {return -1;}
+        if (a[1].innerText > b[1].innerText) {return 1;}
+        return 0;
+    });
+    const sortedTblDest = Object.entries(tblDest).sort((a, b) => {
+        if (a[1].innerText < b[1].innerText) {return -1;}
+        if (a[1].innerText > b[1].innerText) {return 1;}
+        return 0;
+    });
+    
+
+    // Et remplissage des selects avant de quitter
+    sortedTblSrc.forEach((value) => {
+        let new_src_opt = document.createElement('option');
+        new_src_opt.value = value[1].value;
+        new_src_opt.innerText = value[1].innerText;
+        source_cat_select.appendChild(new_src_opt);
+    })
+    sortedTblDest.forEach((value) => {
+        let new_dest_opt = document.createElement('option');
+        new_dest_opt.value = value[1].value;
+        new_dest_opt.innerText = value[1].innerText;
+        source_cat_select.appendChild(new_dest_opt);
+    })
+
+}
--- pages_and_sections/page_categories.php
+++ pages_and_sections/page_categories.php
@@ -7,7 +7,8 @@
     $rqt_listing_categories = "SELECT T.term_id, T.name
     FROM `wp_terms` AS T
     LEFT JOIN `wp_term_taxonomy` AS X ON T.term_id = X.term_id
-    WHERE X.taxonomy = 'category'";
+    WHERE X.taxonomy = 'category'
+    ORDER BY T.name ASC";
 
     $resultat_listing_categories = $wpdb->get_results($rqt_listing_categories);
 
@@ -18,7 +19,7 @@
 <div class="JTGH_WPHGP_categories_layout">
     <div class="JTGH_WPHGP_center_div">
         <h2>Ensemble des catégories</h2>
-        <select class="JTGH_WPHGP_select_cat_layout" name="categories_source" id="categories_source" multiple>
+        <select class="JTGH_WPHGP_select_cat_layout" name="JTGH_WPHGP_categories_source" id="JTGH_WPHGP_categories_source" multiple>
             <?php
                 foreach($resultat_listing_categories as $row) {
                     echo '<option value="'.$row->term_id.'">'.$row->name.'</option>';
@@ -27,14 +28,14 @@
         </select>
     </div>
     <div class="JTGH_WPHGP_center_div">
-        <button class="JTGH_WPHGP_cat_btn_bascul">&gt;&gt;</button>
-        <button class="JTGH_WPHGP_cat_btn_bascul">&gt;</button>
-        <button class="JTGH_WPHGP_cat_btn_bascul">&lt;</button>
-        <button class="JTGH_WPHGP_cat_btn_bascul">&lt;&lt;</button>
+        <button class="JTGH_WPHGP_cat_btn_bascul" onclick="JTGH_WPHGP_handleCategoriesBasc('>>')">&gt;&gt;</button>
+        <button class="JTGH_WPHGP_cat_btn_bascul" onclick="JTGH_WPHGP_handleCategoriesBasc('>')">&gt;</button>
+        <button class="JTGH_WPHGP_cat_btn_bascul" onclick="JTGH_WPHGP_handleCategoriesBasc('<')">&lt;</button>
+        <button class="JTGH_WPHGP_cat_btn_bascul" onclick="JTGH_WPHGP_handleCategoriesBasc('<<')">&lt;&lt;</button>
     </div>
     <div class="JTGH_WPHGP_center_div">
         <h2>Ensemble à prendre en compte</h2>
-        <select class="JTGH_WPHGP_select_cat_layout" name="categories_dest" id="categories_dest" multiple>
+        <select class="JTGH_WPHGP_select_cat_layout" name="JTGH_WPHGP_categories_dest" id="JTGH_WPHGP_categories_dest" multiple>
         </select>
     </div>
 </div>

4193b8b2370257ca7b567eec751dec6d6795c10c
JeromeTGH <jerome.tomski@gmx.fr>
2024-03-03 13:34:16 +0100

    Création logique d'enregistrement de nouvelles couleurs de catégories, dans page 'admin categories'

--- load/install.php
+++ load/install.php
@@ -15,12 +15,20 @@
         if (is_plugin_active($pluginName)) {
             wp_die("is_plugin_active");
         }
+
+        // Création d'une liste de couleurs "vide"
+        $liste_de_couleurs = array();
+        $all_categories_color = new stdClass();
+        $all_categories_color->cat_id = 0;
+        $all_categories_color->couleur = JTGH_WPHGP_DEFAULT_CATEGORY_COLOR;
+        $all_categories_color->affichage = true;
+        $liste_de_couleurs[] = $all_categories_color;
     
         // Création/enregistrement des options
         JTGH_write_option('activation_date', time());
         JTGH_write_option('shortcode_name', JTGH_WPHGP_SHORTCODE_NAME);
         JTGH_write_option('categories_a_afficher', '[]');
-        JTGH_write_option('couleurs_des_categories', '{"0": "'.JTGH_WPHGP_DEFAULT_CATEGORY_COLOR.'"}');
+        JTGH_write_option('couleurs_des_categories', json_encode($liste_de_couleurs));
 
     }
 
--- pages_and_sections/page_couleurs.php
+++ pages_and_sections/page_couleurs.php
@@ -14,17 +14,50 @@
     // Récupération de la liste des couleurs de catégories, enregistrée dans les options de la bdd
     $tblCouleursDesCategories = json_decode(JTGH_read_option('couleurs_des_categories'));
 
+    // Récupération de la liste des catégories choisies, enregistrée dans les options de la bdd
+    $tblCategoriesChoisies = json_decode(JTGH_read_option('categories_a_afficher'));
+
+    // Parcours des catégories choisies, pour voir si elles ont toutes une couleur assignée (sinon, on leur met celle de base, par défaut)
+    $modif_sur_liste_de_couleurs = false;
+    foreach($tblCategoriesChoisies as $cat_choisie_id) {
+        $couleur_trouvee = false;
+        foreach($tblCouleursDesCategories as $couleur_avec_categorie) {
+            if($couleur_avec_categorie->cat_id == $cat_choisie_id) {
+                $couleur_trouvee = true;
+            }
+        }
+        if(!$couleur_trouvee) {
+            $modif_sur_liste_de_couleurs = true;
+            $new_categories_color = new stdClass();
+            $new_categories_color->cat_id = $cat_choisie_id;
+            $new_categories_color->couleur = JTGH_WPHGP_DEFAULT_CATEGORY_COLOR;
+            $new_categories_color->affichage = true;
+            $tblCouleursDesCategories[] = $new_categories_color;
+        }
+    }
+
+    // S'il y a des catégories déselectionnées, alors on les "masque"
+    for($i=0 ; $i < count($tblCouleursDesCategories); $i++) {
+        if(!in_array($tblCouleursDesCategories[$i]->cat_id, $tblCategoriesChoisies) && $tblCouleursDesCategories[$i]->cat_id != 0) {
+            $modif_sur_liste_de_couleurs = true;
+            $tblCouleursDesCategories[$i]->affichage = false;
+        }
+    }  
+
+    // Si la liste des couleurs de catégories a été modifiée, alors on enregistre les changements, et on la recharge
+    if($modif_sur_liste_de_couleurs) {
+        JTGH_write_option('couleurs_des_categories', json_encode($tblCouleursDesCategories));
+        $tblCouleursDesCategories = json_decode(JTGH_read_option('couleurs_des_categories'));
+    }
 
 
 
 
 
 
-
-    
-
-
-    echo json_encode($tblCouleursDesCategories);
+    echo '<br>';
+    echo '<strong>Tableau des couleurs</strong> = '.json_encode($tblCouleursDesCategories).'<br>';
+    echo '<strong>Catégories sélectionnées</strong> = '.json_encode($tblCategoriesChoisies).'<br>';
 
 
 

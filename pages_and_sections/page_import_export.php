88ec7b9ecc2e031aed2ebaaee43c97235087895e
JeromeTGH <jerome.tomski@gmx.fr>
2024-03-04 18:42:32 +0100

    Création d'un système d'export, au format JSON

--- pages_and_sections/page_import_export.php
+++ pages_and_sections/page_import_export.php
@@ -6,10 +6,36 @@
 
     // Si cliqué sur "export json"
     if (isset($_POST['JTGH_WPHGP_export_json'])) {
+
         ob_clean();
+
+        // Paramètres de base de l'export
         $output_filename = 'JTGH_WPHGP_export.json';
-        $donnees_a_enregistrer = ['param1' => 42, 'param2' => 'test'];      // Pour test de création objet json = {"param1":42,"param2":"test"})
         $date_du_jour = date("Y-m-d");
+
+        // Chargement des options
+        $activation_date = JTGH_read_option('activation_date');
+        $shortcode_name = JTGH_read_option('shortcode_name');
+        $categories_a_afficher = JTGH_read_option('categories_a_afficher');
+        $couleurs_des_categories = JTGH_read_option('couleurs_des_categories');
+        $nbre_d_articles_par_page = JTGH_read_option('nbre_d_articles_par_page');
+        $nbre_de_colonnes_d_affichage = JTGH_read_option('nbre_de_colonnes_d_affichage');
+        $afficher_metadonnees = JTGH_read_option('afficher_metadonnees');
+        $longueur_maxi_extract = JTGH_read_option('longueur_maxi_extract');
+
+        // Construction du tableau associatif, qui sera ensuite transformé en objet json
+        $donnees_a_enregistrer = [
+            'activation_date' => $activation_date,
+            'shortcode_name' => $shortcode_name,
+            'categories_a_afficher' => $categories_a_afficher,
+            'couleurs_des_categories' => $couleurs_des_categories,
+            'nbre_d_articles_par_page' => $nbre_d_articles_par_page,
+            'nbre_de_colonnes_d_affichage' => $nbre_de_colonnes_d_affichage,
+            'afficher_metadonnees' => $afficher_metadonnees,
+            'longueur_maxi_extract' => $longueur_maxi_extract
+        ];
+
+        // Envoi de l'entête et des données au format JSON
         header('Pragma: public');
         header('Expires: 0');
         header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
@@ -17,6 +43,8 @@
         header('Content-type: application/json');
         header('Content-Disposition: attachment; filename='.$date_du_jour.' '.$output_filename);
         echo json_encode($donnees_a_enregistrer);
+
+        // Clotûre de l'envoi
         ob_flush();
         exit;
     }
